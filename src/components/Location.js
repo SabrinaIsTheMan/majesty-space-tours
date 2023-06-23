2a3b96ebf4d90f2b27e2b6e16901111db82c09d3
Tr1st0n <tristonxmoulton@hotmail.com>
2023-06-22 21:46:24 -0400

    Add image API

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,7 @@
 import '../styles/Location.css';
 import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 function Location({ selectedTour, handleDateClick } ) {
 
@@ -9,13 +9,79 @@ function Location({ selectedTour, handleDateClick } ) {
 
     const [ image, setImage ] = useState("");
 
-    // useEffect (() => {
-    //     axios({
+    useEffect (() => {
+        if(location === 'Gale Crater'){
+            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',{
+                params: {
+                sol: 3780,
+                camera:'MAST',
+                page: 1,
+                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                }
+              })
+              .then((jsonResult) => {
+                console.log(jsonResult.data.photos[7])
+                setImage(jsonResult.data.photos[7].img_src)
+              })
+              .catch((error)=> {
+                alert('Error!')
+              })
+        }
+        if(location === 'Gusev Crater'){
+            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos',{
+                params: {
+                sol: 1274,
+                camera:'PANCAM',
+                page: 1,
+                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                }
+              })
+              .then((jsonResult) => {
+                console.log(jsonResult.data.photos[0])
+                setImage(jsonResult.data.photos[0].img_src)
+              })
+              .catch((error)=> {
+                alert('Error!')
+              })
+        }
+    
+        if(location === 'Jezero Crater'){
+            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos',{
+                params: {
+                sol: 420,
+                camera:'NAVCAM_RIGHT',
+                page: 1,
+                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                }
+              })
+              .then((jsonResult) => {
+                console.log(jsonResult.data.photos[22])
+                setImage(jsonResult.data.photos[22].img_src)
+              })
+              .catch((error)=> {
+                alert('Error!')
+              })
+        }
 
-    //         // image api here with useParams
-    //         // setImage(url)
-
-    //     })}, [])
+        if(location === 'Meridiani Planum'){
+            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos',{
+                params: {
+                sol: 4558,
+                camera:'PANCAM',
+                page: 1,
+                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                }
+              })
+              .then((jsonResult) => {
+                console.log(jsonResult.data.photos[15])
+                setImage(jsonResult.data.photos[15].img_src)
+              })
+              .catch((error)=> {
+                alert('Error!')
+              })
+        }
+    
+    }, [location])
 
     return (
 
