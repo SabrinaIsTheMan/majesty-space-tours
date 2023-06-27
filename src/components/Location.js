3be2a80f3a432c8744c68767290870d2529d52e5
SabrinaIsTheMan <sabrina.yt.man@gmail.com>
2023-06-27 18:47:26 -0400

    add asteroids API, add .env file, finalize About page layout, add links to GitHubs, add Sabrina icon

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,24 +3,24 @@ import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Location({ selectedTour, handleDateClick } ) {
+function Location({ selectedTour } ) {
 
     const { location } = useParams();
-
     const [ image, setImage ] = useState("");
+
+  const apiURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
 
     useEffect (() => {
         if(location === 'Gale Crater'){
-            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',{
+            axios(`${apiURL}curiosity/photos`, {
                 params: {
                 sol: 3780,
                 camera:'MAST',
                 page: 1,
-                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                api_key: process.env.REACT_APP_API_KEY
                 }
               })
               .then((jsonResult) => {
-                console.log(jsonResult.data.photos[7])
                 setImage(jsonResult.data.photos[7].img_src)
               })
               .catch((error)=> {
@@ -28,34 +28,32 @@ function Location({ selectedTour, handleDateClick } ) {
               })
         }
         if(location === 'Gusev Crater'){
-            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos',{
+          axios(`${apiURL}spirit/photos`,{
                 params: {
                 sol: 1274,
                 camera:'PANCAM',
                 page: 1,
-                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+              api_key: process.env.REACT_APP_API_KEY
                 }
               })
               .then((jsonResult) => {
-                console.log(jsonResult.data.photos[0])
                 setImage(jsonResult.data.photos[0].img_src)
               })
               .catch((error)=> {
                 alert('Error!')
               })
         }
-    
+
         if(location === 'Jezero Crater'){
-            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos',{
+          axios(`${apiURL}perseverance/photos`,{
                 params: {
                 sol: 420,
                 camera:'NAVCAM_RIGHT',
                 page: 1,
-                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                api_key: process.env.REACT_APP_API_KEY
                 }
               })
               .then((jsonResult) => {
-                console.log(jsonResult.data.photos[22])
                 setImage(jsonResult.data.photos[22].img_src)
               })
               .catch((error)=> {
@@ -64,23 +62,22 @@ function Location({ selectedTour, handleDateClick } ) {
         }
 
         if(location === 'Meridiani Planum'){
-            axios('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos',{
+          axios(`${apiURL}opportunity/photos`,{
                 params: {
                 sol: 4558,
                 camera:'PANCAM',
                 page: 1,
-                api_key: 'dma6KHcns7nZ3JNb1mlfCB0iDvzaO3rMa0WvqBZ3'
+                api_key: process.env.REACT_APP_API_KEY
                 }
               })
               .then((jsonResult) => {
-                console.log(jsonResult.data.photos[15])
                 setImage(jsonResult.data.photos[15].img_src)
               })
               .catch((error)=> {
                 alert('Error!')
               })
         }
-    
+
     }, [location])
 
     return (
@@ -99,7 +96,7 @@ function Location({ selectedTour, handleDateClick } ) {
                 <Link to="dates">
                     <button type="button"
                         value={selectedTour.locationName}
-                        onClick={(event) => { handleDateClick(event) }}>Tour Dates</button>
+                    >Tour Dates</button>
                 </Link>
 
                 <Link to="/tours">
